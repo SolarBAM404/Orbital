a10c9c80957a246a1e953d8afcf6b2f8bd45e00c
Solar M. <solar@lunar-dev.com>
2025-02-17 01:54:51 +0000

    Example file loading

--- OrbitalCore/Program.cs
+++ OrbitalCore/Program.cs
@@ -1,3 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using OrbitalCore;
+
+if (args.Length == 0)
+{
+    string input = "";
+    while (input != "exit")
+    {
+        Console.Write("> ");
+        input = Console.ReadLine();
+        if (input == "exit")
+        {
+            break;
+        }
+        List<object?> results = Evaluator.EvaluateAndExecute(input);
+    }
+
+    return;
+}
+
+// if path is provided, read the file and execute the code
+
+string path = args[0];
+string code = File.ReadAllText(path);
+List<object?> fileResults = Evaluator.EvaluateAndExecute(code);
